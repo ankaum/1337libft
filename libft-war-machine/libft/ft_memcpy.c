2602235337eefe791cda5f8de52421ac9d89854e
ankaum <anasszg@gmail.com>
2021-12-24 08:18:17 +0100

    final

--- libft-war-machine/libft/ft_memcpy.c
+++ libft-war-machine/libft/ft_memcpy.c
@@ -6,11 +6,11 @@
 /*   By: azouaiga <azouaiga@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:55:06 by azouaiga          #+#    #+#             */
-/*   Updated: 2021/12/15 05:43:11 by azouaiga         ###   ########.fr       */
+/*   Updated: 2021/12/24 05:42:16 by azouaiga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h" /*temporarily*/
+#include"libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
